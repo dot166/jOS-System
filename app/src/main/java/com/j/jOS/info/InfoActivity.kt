012047f3e0b7ceb664db963072ee40e71e55ff6d
_______166 <62702353+jf916@users.noreply.github.com>
2023-10-17 20:23:40 +0100

    Update InfoActivity.kt

--- app/src/main/java/com/j/jOS/info/InfoActivity.kt
+++ app/src/main/java/com/j/jOS/info/InfoActivity.kt
@@ -13,11 +13,6 @@ class InfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val lineageos = Build.LINEAGEOS_VERSION
-        val substring = "beyond1lte"
-        val usingjOSdevice = lineageos.contains(substring)
-        Log.i(ContentValues.TAG, lineageos)
-        Log.i(ContentValues.TAG, usingjOSdevice.toString())
         setContentView(R.layout.activity_about)
     }
 }
