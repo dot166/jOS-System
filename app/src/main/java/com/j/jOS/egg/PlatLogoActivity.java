3ba97dca3c4f6b90ee34fcc3722cc070c1fee9c4
_______166 <62702353+jf916@users.noreply.github.com>
2023-10-19 22:30:46 +0200

    Update PlatLogoActivity.java

--- app/src/main/java/com/j/jOS/egg/PlatLogoActivity.java
+++ app/src/main/java/com/j/jOS/egg/PlatLogoActivity.java
@@ -30,6 +30,9 @@ import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.android_k.egg.R;
+// temp import desert cake for testing
+import com.android_k.egg.DesertCake;
 
 import com.dede.basic.SpUtils;
 import com.dede.basic.TransformationMethodUtils;
