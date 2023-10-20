c0078cae9e9ce781f4287807ef0418ccf5d6161d
_______166 <62702353+jf916@users.noreply.github.com>
2023-10-20 17:58:30 +0100

    Update EasterEggsActivity.kt

--- app/src/main/java/com/j/jOS/egg/menu/EasterEggsActivity.kt
+++ app/src/main/java/com/j/jOS/egg/menu/EasterEggsActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.j.jOS.R
-import com.com.j.jOS.databinding.ActivityEasterEggsBinding
-import com.com.j.jOS.egg.menu.util.EdgeUtils
+import com.j.jOS.databinding.ActivityEasterEggsBinding
+import com.j.jOS.egg.menu.util.EdgeUtils
 import com.j.jOS.egg.menu.util.ThemeUtils
 import com.j.jOS.egg.menu.views.main.StartupPage
 
