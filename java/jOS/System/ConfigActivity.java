81fbcc979b766e2d6ed2ca7e937a7fbf05d55efc
_______166 <retromac9x2@gmail.com>
2024-03-15 21:07:20 +0000

    move to sql
    this system app holds the database

--- java/jOS/System/ConfigActivity.java
+++ java/jOS/System/ConfigActivity.java
@@ -1,8 +1,211 @@
 package jOS.System;
 
-import jOS.Core.SDKConfigActivity;
+import static jOS.Core.ActionBar.actionBarConfig;
+import static jOS.Core.ThemeEngine.currentTheme;
+import static jOS.Core.ThemeEngine.getSystemTheme;
+import static jOS.Core.ThemeEngine.getThemeFromDB1;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.VisibleForTesting;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceFragmentCompat.OnPreferenceStartFragmentCallback;
+import androidx.preference.PreferenceFragmentCompat.OnPreferenceStartScreenCallback;
+import androidx.preference.PreferenceScreen;
+
+import java.util.Objects;
 
 /**
- * Settings activity.
+ * Settings activity for Launcher. Currently implements the following setting: Allow rotation
  */
-public class ConfigActivity extends SDKConfigActivity {}
+public class ConfigActivity extends AppCompatActivity
+        implements OnPreferenceStartFragmentCallback, OnPreferenceStartScreenCallback,
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+    public static AppCompatActivity mContext;
+
+    public static final String EXTRA_FRAGMENT_ARG_KEY = ":settings:fragment_args_key";
+    public static final String SAVE_HIGHLIGHTED_KEY = "android:preference_highlighted";
+
+    @VisibleForTesting
+    static final String EXTRA_FRAGMENT = ":settings:fragment";
+    @VisibleForTesting
+    static final String EXTRA_FRAGMENT_ARGS = ":settings:fragment_args";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(getSystemTheme(this));
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        mContext = this;
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        Intent intent = getIntent();
+        actionBarConfig(jOS.Core.R.string.settings_name, jOS.Core.R.drawable.ic_launcher_j, false, this, "");
+
+        if (savedInstanceState == null) {
+            Bundle args = intent.getBundleExtra(EXTRA_FRAGMENT_ARGS);
+            if (args == null) {
+                args = new Bundle();
+            }
+
+            String prefKey = intent.getStringExtra(EXTRA_FRAGMENT_ARG_KEY);
+            if (!TextUtils.isEmpty(prefKey)) {
+                args.putString(EXTRA_FRAGMENT_ARG_KEY, prefKey);
+            }
+
+            final FragmentManager fm = getSupportFragmentManager();
+            final Fragment f = fm.getFragmentFactory().instantiate(getClassLoader(),
+                    getPreferenceFragment());
+            f.setArguments(args);
+            // Display the fragment as the main content.
+            fm.beginTransaction().replace(R.id.content_frame, f).commit();
+        }
+
+    }
+
+    public static void reboot(AppCompatActivity context){
+        Intent intent = context.getIntent();
+        context.finish();
+        context.startActivity(intent);
+    }
+
+    /**
+     * Obtains the preference fragment to instantiate in this activity.
+     *
+     * @return the preference fragment class
+     * @throws IllegalArgumentException if the fragment is unknown to this activity
+     */
+    private String getPreferenceFragment() {
+        String preferenceFragment = getIntent().getStringExtra(EXTRA_FRAGMENT);
+        String defaultFragment = getString(R.string.settings_fragment_name);
+
+        if (TextUtils.isEmpty(preferenceFragment)) {
+            return defaultFragment;
+        } else if (!preferenceFragment.equals(defaultFragment)) {
+            throw new IllegalArgumentException(
+                    "Invalid fragment for this activity: " + preferenceFragment);
+        } else {
+            return preferenceFragment;
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+    }
+
+    private boolean startPreference(String fragment, Bundle args, String key) {
+        if (getSupportFragmentManager().isStateSaved()) {
+            // Sometimes onClick can come after onPause because of being posted on the handler.
+            // Skip starting new preferences in that case.
+            return false;
+        }
+        final FragmentManager fm = getSupportFragmentManager();
+        final Fragment f = fm.getFragmentFactory().instantiate(getClassLoader(), fragment);
+        if (f instanceof DialogFragment) {
+            f.setArguments(args);
+            ((DialogFragment) f).show(fm, key);
+        } else {
+            startActivity(new Intent(this, ConfigActivity.class)
+                    .putExtra(EXTRA_FRAGMENT, fragment)
+                    .putExtra(EXTRA_FRAGMENT_ARGS, args));
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceStartFragment(
+            PreferenceFragmentCompat preferenceFragment, Preference pref) {
+        return startPreference(pref.getFragment(), pref.getExtras(), pref.getKey());
+    }
+
+    @Override
+    public boolean onPreferenceStartScreen(PreferenceFragmentCompat caller, PreferenceScreen pref) {
+        Bundle args = new Bundle();
+        args.putString(PreferenceFragmentCompat.ARG_PREFERENCE_ROOT, pref.getKey());
+        return startPreference(getString(R.string.settings_fragment_name), args, pref.getKey());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        if (!Objects.equals(currentTheme, getThemeFromDB1(this))) {
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * This fragment shows the launcher preferences.
+     */
+    public static class LauncherSettingsFragment extends PreferenceFragmentCompat {
+
+        private String mHighLightKey;
+        private boolean mPreferenceHighlighted = false;
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            final Bundle args = getArguments();
+            mHighLightKey = args == null ? null : args.getString(EXTRA_FRAGMENT_ARG_KEY);
+            if (rootKey == null && !TextUtils.isEmpty(mHighLightKey)) {
+                rootKey = getParentKeyForPref(mHighLightKey);
+            }
+
+            if (savedInstanceState != null) {
+                mPreferenceHighlighted = savedInstanceState.getBoolean(SAVE_HIGHLIGHTED_KEY);
+            }
+            setPreferencesFromResource(R.xml.launcher_preferences, rootKey);
+        }
+
+        @Override
+        public void onDestroyView() {
+            super.onDestroyView();
+        }
+
+
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            View listView = getListView();
+            final int bottomPadding = listView.getPaddingBottom();
+            listView.setOnApplyWindowInsetsListener((v, insets) -> {
+                v.setPadding(
+                        v.getPaddingLeft(),
+                        v.getPaddingTop(),
+                        v.getPaddingRight(),
+                        bottomPadding + insets.getSystemWindowInsetBottom());
+                return insets.consumeSystemWindowInsets();
+            });
+        }
+
+        @Override
+        public void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            outState.putBoolean(SAVE_HIGHLIGHTED_KEY, mPreferenceHighlighted);
+        }
+
+        protected String getParentKeyForPref(String key) {
+            return null;
+        }
+    }
+}
