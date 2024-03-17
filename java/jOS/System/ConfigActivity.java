2f6d5790299d08ce9eb54feeac735820bf41fb09
_______166 <retromac9x2@gmail.com>
2024-03-17 17:39:06 +0000

    fixes

--- java/jOS/System/ConfigActivity.java
+++ java/jOS/System/ConfigActivity.java
@@ -1,5 +1,10 @@
 package jOS.System;
 
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.preference.Preference;
+
 import jOS.Core.jConfigActivity;
 
 public class ConfigActivity extends jConfigActivity {
@@ -19,6 +24,21 @@ public class ConfigActivity extends jConfigActivity {
         @Override
         public int preferenceXML() {
             return R.xml.sdk_preferences;
+        }
+        @Override
+        protected boolean extraPrefs(Preference preference) {
+            Log.i("Preference Logging", preference.getKey());
+            switch (preference.getKey()) {
+                case "pref_themeengine":
+                    Log.i("Preference Logging", "pref_themeengine Found!!!!");
+                    preference.setOnPreferenceClickListener(p -> {
+                        Intent intent = new Intent("jOS.ThemeEngine.CONFIG");
+                        startActivity(intent);
+                        return true;
+                    });
+                    return true;
+            }
+            return true;
         }
     }
 }
