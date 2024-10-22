70e1bf77c16ee5bcd95cd9fb7035df23714069c8
._______166 <retromac9x2@gmail.com>
2024-10-22 13:11:42 +0100

    jOS-System: Remove Audio Prefs
    jOS 15.1 does not have this feature because it is useless

--- java/jOS/System/ConfigActivity.java
+++ java/jOS/System/ConfigActivity.java
@@ -17,16 +17,6 @@ import jOS.Core.jConfigActivity;
 
 public class ConfigActivity extends jConfigActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        AudioManager am = new AudioManager(this);
-        boolean oldValue = am.getSafeMediaVolumeState();
-        SharedPreferences.Editor prefs = PreferenceManager.getDefaultSharedPreferences(this).edit();
-        prefs.putBoolean("pref_safevol", oldValue);
-        prefs.commit();
-    }
-    @Override
     public int preferenceFragmentValue() {
         return R.string.sdk_settings_fragment_name;
     }
@@ -38,26 +28,6 @@ public class ConfigActivity extends jConfigActivity {
         @Override
         public int preferenceXML() {
             return R.xml.sdk_preferences;
-        }
-        @Override
-        public void initPreference(String rootKey) {
-            super.initPreference(rootKey);
-            PreferenceScreen screen = this.getPreferenceScreen();
-            addPreferencesFromResource(R.xml.audio_preference);
-
-            for(int i = screen.getPreferenceCount() - 1; i >= 0; --i) {
-                Preference preference = screen.getPreference(i);
-                if (Objects.equals(preference.getKey(), "audio_category")) {
-                    PreferenceCategory category = (PreferenceCategory)preference;
-
-                    for(int i2 = category.getPreferenceCount() - 1; i2 >= 0; --i2) {
-                        Preference preference2 = category.getPreference(i2);
-                        if (!this.configPreference(preference2)) {
-                            category.removePreference(preference2);
-                        }
-                    }
-                }
-            }
         }
         @Override
         protected boolean extraPrefs(Preference preference) {
@@ -72,7 +42,7 @@ public class ConfigActivity extends jConfigActivity {
                     });
                     return true;
             }
-            return audioprefs.parseAudioPrefs(preference);
+            return true;
         }
     }
 }
