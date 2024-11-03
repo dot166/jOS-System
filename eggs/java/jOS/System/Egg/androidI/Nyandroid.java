e4db34f0aba641d3690f651311680d6247cf3fd2
._______166 <62702353+dot166@users.noreply.github.com>
2024-11-03 13:13:51 +0000

    Fix Bug in Nyandroid.java

--- eggs/java/jOS/System/Egg/androidI/Nyandroid.java
+++ eggs/java/jOS/System/Egg/androidI/Nyandroid.java
@@ -235,15 +235,6 @@ public class Nyandroid extends Activity {
         super.onResume();
         mBoard = new Board(this, null);
         setContentView(mBoard);
-
-        mBoard.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-            @Override
-            public void onSystemUiVisibilityChange(int vis) {
-                if (0 == (vis & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)) {
-                    Nyandroid.this.finish();
-                }
-            }
-        });
     }
 
     @Override
