987a371a2ccc59be15c7873c72835f029449633f
._______166 <62702353+dot166@users.noreply.github.com>
2024-11-03 15:44:09 +0000

    Update ConfigActivity.java

--- java/jOS/System/ConfigActivity.java
+++ java/jOS/System/ConfigActivity.java
@@ -17,10 +17,10 @@ import jOS.Core.jConfigActivity;
 
 public class ConfigActivity extends jConfigActivity {
     @Override
-    public int preferenceFragmentValue() {
-        return R.string.sdk_settings_fragment_name;
+    public jLIBSettingsFragment preferenceFragment() {
+        return new jSDKConfigFragment();
     }
-    public static class jSDKConfigFragment extends LauncherSettingsFragment {
+    public static class jSDKConfigFragment extends jLIBSettingsFragment {
         @Override
         public boolean isLIBConfig() {
             return true;
