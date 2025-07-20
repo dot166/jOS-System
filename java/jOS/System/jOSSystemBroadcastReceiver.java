83851829ca90fe0a7fdaf25e084919a20740888d
._______166 <retromac9x2@gmail.com>
2025-07-20 15:35:16 +0100

    fix the feature not working

--- java/jOS/System/jOSSystemBroadcastReceiver.java
+++ java/jOS/System/jOSSystemBroadcastReceiver.java
@@ -5,46 +5,54 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.ext.settings.ExtSettings;
 import android.media.AudioManager;
-import android.provider.Settings;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 public class jOSSystemBroadcastReceiver extends BroadcastReceiver {
     private static final String TAG = jOSSystemBroadcastReceiver.class.getSimpleName();
+    private int mLastZenMode;
 
     @Override
     public void onReceive(final Context context, @SuppressLint("UnsafeIntentLaunch") final Intent intent) {
         final String action = intent.getAction();
         if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
             Log.i(TAG, "Boot has been completed");
-            IntentFilter intentFilter = new IntentFilter(NotificationManager.ACTION_INTERRUPTION_FILTER_CHANGED);
 
-            jOSSystemBroadcastReceiver mReceiver = new jOSSystemBroadcastReceiver();
-            context.registerReceiver(mReceiver, intentFilter);
-        } else if (NotificationManager.ACTION_INTERRUPTION_FILTER_CHANGED.equals(action)) {
-            Log.i(TAG, "DnD Changed (i think???)");
-            // force enable it
-            //if (ExtSettings.ENABLE_VIBRATE_ON_DO_NOT_DISTURB.get(context) == true) {
-                AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-                final NotificationManager manager = context.getSystemService(NotificationManager.class);
-                final int zenMode = manager.getZenMode();
+            Handler handler = new Handler(Looper.getMainLooper());
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    // force enable it
+                    //if (ExtSettings.ENABLE_VIBRATE_ON_DO_NOT_DISTURB.get(context) == true) {
+                        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+                        final NotificationManager manager = context.getSystemService(NotificationManager.class);
+                        final int zenMode = manager.getCurrentInterruptionFilter()                                      ;
 
-                switch (zenMode) {
-                    case Settings.Global.ZEN_MODE_ALARMS:
-                    case Settings.Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS:
-                    case Settings.Global.ZEN_MODE_NO_INTERRUPTIONS:
-                        audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
-                        break;
-                    case Settings.Global.ZEN_MODE_OFF:
-                    default:
-                        audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
-                        break;
+                        if (mLastZenMode != zenMode) {
+                            Log.i(TAG, "DnD Changed (i think???)");
+                            switch (zenMode) {
+                                case NotificationManager.INTERRUPTION_FILTER_ALARMS:
+                                case NotificationManager.INTERRUPTION_FILTER_PRIORITY:
+                                case NotificationManager.INTERRUPTION_FILTER_NONE:
+                                case NotificationManager.INTERRUPTION_FILTER_UNKNOWN:
+                                    audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
+                                    break;
+                                case NotificationManager.INTERRUPTION_FILTER_ALL:
+                                    audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+                                    break;
+                            }
+                            mLastZenMode = zenMode;
+                        }
+                    //} else {
+                    //    Log.i(TAG, "Vibrate on DnD is Disabled by the user");
+                    //}
+                    handler.postDelayed(this, 100); // Poll every 100 milliseconds
                 }
-            //} else {
-                //Log.i(TAG, "Vibrate on DnD is Disabled by the user");
-            //}
+            };
+            handler.post(runnable);
         }
     }
 }
